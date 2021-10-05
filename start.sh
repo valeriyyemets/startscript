5ff84eebd8a952031e039597f9dbf37d871d451b
Valeriy Yemets <valeriy.yemets@gmail.com>
2021-10-05 20:00:24 +0000

    add start portainer

--- start.sh
+++ start.sh
@@ -8,3 +8,11 @@ yum-config-manager \
 yum install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
 systemctl start docker
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+
