af22b87f4fe3d83d5a68bf39773adea14058bb32
Valeriy Yemets <valeriy.yemets@gmail.com>
2021-10-05 19:23:21 +0000

    start docker

--- start.sh
+++ start.sh
@@ -6,3 +6,5 @@ yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io -y
+systemctl enable docker
+systemctl start docker
